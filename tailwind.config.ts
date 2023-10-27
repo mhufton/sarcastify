8a4ebac67dd5e452707b6bdb61fff0fee31c0d15
Mike Hufton <mike.hufton@postcodelottery.co.uk>
2023-10-27 13:56:35 +0100

    fadein/out of the button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,24 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      // that is animation class
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+        fadeOut: "fadeOut 1s ease-in-out",
+      },
+
+      // that is actual animation
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        fadeOut: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
     },
   },
   plugins: [],
