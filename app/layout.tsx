0e83b96766e425dffbc322ef29da3b66aad1332f
Mike Hufton <mikehufton@mikes-macbook.home>
2023-11-02 12:22:39 +0000

    added meta tags and adsense info

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>SaRcAsTiFy</title>
+        <meta name="Sarcastify" content="A sarcastic text generator" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8508403078221807"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
